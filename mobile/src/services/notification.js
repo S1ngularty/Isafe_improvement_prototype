7c4086a582bb73c8535fd672e03f8ead0a2336cf
S1ngularty <asherxd10245@gmail.com>
2026-06-19 08:44:12 +0800

    registerPushToken

---
+++ mobile/src/services/notification.js
@@ -0,0 +1,55 @@
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export async function registerForPushNotificationsAsync() {
+  if (!Device.isDevice) {
+    console.log('Must use a physical device');
+    return null;
+  }
+
+  const { status: existingStatus } =
+    await Notifications.getPermissionsAsync();
+
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+    const { status } =
+      await Notifications.requestPermissionsAsync();
+
+    finalStatus = status;
+  }
+
+  if (finalStatus !== 'granted') {
+    console.log('Permission denied');
+    return null;
+  }
+
+  try {
+    const projectId =
+      Constants.expoConfig?.extra?.eas?.projectId ||
+      Constants.easConfig?.projectId;
+
+    const token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId,
+      })
+    ).data;
+
+    console.log('Expo Push Token:', token);
+
+    return token;
+  } catch (error) {
+    console.error('Token error:', error);
+    return null;
+  }
+}
